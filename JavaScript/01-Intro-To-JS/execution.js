297c5d110356300b2d92d753a97df403259f7255
harsh-sangwan2002 <hsangwan2002@gmail.com>
2026-06-25 01:08:31 +0530

    Class 01 - Compiled Vs Interpreted languages done

---
+++ JavaScript/01-Intro-To-JS/execution.js
@@ -0,0 +1,12 @@
+/* 1. Compiled Languages: Source Code -> Compiler -> Machine Code(.exe) -> Execution 
+Languages: C, C++, Rust, Go
+
+2. Interpreted Languages: Source Code -> Interpreter -> Execute line by line
+Languages: Ruby, Bash, Old JS
+
+3. JIT Compiled: Source Code -> ByteCode -> JIT Compiler -> Machine Code -> Execution
+Languages: Java, JS, C#, Kotlin
+
+In terms of execution speed
+C/C++ > Rust > Java > Go > JS > Python
+*/
