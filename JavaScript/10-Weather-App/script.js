480677fe85cff17759d9f474103dd00708cfbd93
harsh-sangwan2002 <hsangwan2002@gmail.com>
2026-06-29 19:24:56 +0530

    feat: added event listener to the input box

--- JavaScript/10-Weather-App/script.js
+++ JavaScript/10-Weather-App/script.js
@@ -11,6 +11,13 @@ button.addEventListener('click', async (e) => {
     await fetchWeatherData(city);
 })
 
+input.addEventListener('keypress', async (e) => {
+    if (e.key == 'Enter') {
+        const city = input.value;
+        await fetchWeatherData(city);
+    }
+})
+
 const fetchWeatherData = async (city) => {
     let url = `https://api.weatherapi.com/v1/current.json?key=fb0b9d625d354cb684e50017251701&q=${city}&aqi=no`;
     const data = await fetch(url);
@@ -19,7 +26,7 @@ const fetchWeatherData = async (city) => {
 }
 
 const updateDOM = (res) => {
-    temp.innerText = res.current.temp_f;
+    temp.innerText = res.current.temp_c + " °C";
     city.innerText = res.location.name;
     condition.innerText = res.current.condition.text;
 }
