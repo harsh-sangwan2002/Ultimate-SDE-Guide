6e4294229f49676ea55421d510594503c8e8de81
harsh-sangwan2002 <hsangwan2002@gmail.com>
2026-08-10 14:23:43 +0530

    Class04 - For Loop done

---
+++ DSA/Intermediate-Refresher/04-For-Loop/Syntax.java
@@ -0,0 +1,25 @@
+public class Syntax {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10;) {
+            System.out.println(i);
+            i++;
+        }
+
+        for (int i = 1; i <= 20;) {
+            System.out.println(i);
+            i++;
+        }
+        for (int i = 1; true;) {
+            System.out.println(i);
+            i++;
+        }
+
+        /*
+         * for(initialization; condition; updation){
+         * // logic
+         * }
+         */
+    }
+}
