f4688a6f278b4913e6427fd3036efc854908267a
harsh-sangwan2002 <hsangwan2002@gmail.com>
2026-08-31 17:46:23 +0530

    Refactor Solution.java to make checkBit and solve methods static

--- DSA/Advanced/DSA-1/02-Bit-Manipulation/Bit-Manipulation-2/SubArrayOR/Solution.java
+++ DSA/Advanced/DSA-1/02-Bit-Manipulation/Bit-Manipulation-2/SubArrayOR/Solution.java
@@ -4,12 +4,12 @@ package SubArrayOR;
 // SC => O(1)
 public class Solution {
 
-    private boolean checkBit(int n, int pos) {
+    private static boolean checkBit(int n, int pos) {
 
         return ((n & (1 << pos)) != 0) ? true : false;
     }
 
-    public int solve(int[] A) {
+    public static int solve(int[] A) {
 
         long ans = 0L, mod = (long) (1e9 + 7);
         int n = A.length;
@@ -23,9 +23,16 @@ public class Solution {
                     idx = j;
 
                 ans = (ans + (n - idx) * 1L * (1 << i)) % mod;
+                System.out.println(ans);
             }
         }
 
         return (int) ans;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = { 1, 2, 3, 4, 5 };
+        solve(arr);
     }
 }
